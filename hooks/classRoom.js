352175094659d7d589e35eefe799f68a84f4577f
Muhamad Chairul Aziz <m.chairul669@gmail.com>
2019-11-19 19:34:27 +0700

    hooks

--- hooks/classRoom.js
+++ hooks/classRoom.js
@@ -27,7 +27,81 @@ module.exports = (app) => ({
             //do something after create request
         },
         patch: async (context) => {
-            //do something after patch request
+            const classData = context.result
+            const {data, params: {headers}} = context
+
+            if ((data.$addToSet && data.$addToSet.studentsId) || (data.$pull && data.$pull.studentsId)) {
+                let studentId = ''
+                if (data.$addToSet) studentId = data.$addToSet.studentsId
+                if (data.$pull) studentId = data.$pull.studentsId
+
+                try {
+                    const studentGroups = await app.getRequester('studentGroup').send({
+                        type: 'find',
+                        query: {
+                            studentId: studentId
+                        }
+                    })
+
+                    if (data.$addToSet) {
+                        if (studentGroups.length == 0) {
+                            const createStudentGroup = await app.getRequester('studentGroup').send({
+                                type: 'create',
+                                body: {
+                                    studentId,
+                                    groupId: classData.groupId,
+                                },
+                                headers
+                            })
+
+                            const addClassOnStudentGroup = await app.getRequester('studentGroup').send({
+                                type: 'patch',
+                                id: createStudentGroup.id,
+                                body: {
+                                    "$addToSet": {
+                                        classRoomsId: classData.id
+                                    }
+                                },
+                                headers
+                            })
+                        } else {
+                            const studentGroup = studentGroups[0]
+                            const addClassOnStudentGroup = await app.getRequester('studentGroup').send({
+                                type: 'patch',
+                                id: studentGroup.id,
+                                body: {
+                                    "$addToSet": {
+                                        classRoomsId: classData.id
+                                    }
+                                },
+                                headers
+                            })
+                        }
+                    } else if (data.$pull) {
+                        const studentGroup = studentGroups[0]
+                        if (studentGroup.classRoomsId.length == 1) {
+                            const removeStudentId = await app.getRequester('studentGroup').send({
+                                type: 'delete',
+                                id: studentGroup.id,
+                                headers
+                            })
+                        } else {
+                        const removeClassOnStudentGroup = await app.getRequester('studentGroup').send({
+                                type: 'patch',
+                                id: studentGroup.id,
+                                body: {
+                                    "$pull": {
+                                        classRoomsId: classData.id
+                                    }
+                                },
+                                headers
+                            }) 
+                        }
+                    }
+                } catch (e) {
+                    console.log(e)
+                }
+            }
         },         
         delete: async (context) => {
             //do something after delete request
